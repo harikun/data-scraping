f41693c2f867ce672a59d10b3e00a8200a9a5f9a
Cipto Suhari <cipto.suheri@gmail.com>
2022-01-29 17:11:36 +0700

    scrape data json from floatreate and create file excel

---
+++ floatrates.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url = 'http://www.floatrates.com/daily/idr.json'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+}
+res = requests.get(url, headers=headers)
+print(res.status_code)
+res_json = res.json()
+
+data_rates = []
+for data in res_json.values():
+    # print(data['code'],  data['name'], data['inverseRate'], data['date'])
+
+    # sorting data
+    data_dict = {
+        'code': data['code'],
+        'name': data['name'],
+        'inverseRate': data['inverseRate'],
+        'date': data['date']
+    }
+    data_rates.append(data_dict)
+print(f'data berhasil di scraping')
+
+#create a excel file
+try:
+    os.mkdir('data_excel')
+except:
+    pass
+
+df = pd.DataFrame(data_rates)
+df.to_excel('data_excel/float_rates.xlsx', index=False)
+
+print('data berhasil di export ke excel')
