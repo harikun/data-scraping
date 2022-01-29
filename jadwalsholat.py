c5a2158eb126a296bd3bc9af5cf667d282b9fc97
Cipto Suhari <cipto.suheri@gmail.com>
2022-01-29 19:19:33 +0700

    scrape data json from pkpu and create file json for jadwal sholat to day untuk wilayah bojonegoro

--- jadwalsholat.py
+++ jadwalsholat.py
@@ -12,3 +12,30 @@ headers = {
 }
 res = requests.get(url, params=params, headers=headers)
 print(res.status_code)
+soup = BeautifulSoup(res.text, 'html.parser')
+jadwal_sholat = soup.find_all('tr', {'class': 'table_highlight'})
+datas = jadwal_sholat[0]
+sholat = {}
+i = 0
+for data in datas:
+    if i == 1:
+        sholat['subuh'] = data.text
+    elif i == 2:
+        sholat['dzuhur'] = data.text
+    elif i == 3:
+        sholat['ashar'] = data.text
+    elif i == 4:
+        sholat['maghrib'] = data.text
+    elif i == 5:
+        sholat['isya'] = data.text
+    i += 1
+# create a json file
+try:
+   os.mkdir('data_json')
+except OSError:
+    pass
+
+with open('data_json/pkpu.json', 'w+') as f:
+    json.dump(sholat, f)
+    f.close()
+print('Data berhasil di simpan di data_json/pkpu.json')
