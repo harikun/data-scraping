b8fee0c5654cea9b36862fc7b143cad90aba03fb
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-01 23:25:44 +0700

    upload result data scraping

--- myanimelist.py
+++ myanimelist.py
@@ -3,21 +3,11 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
-limit = 0
-no = 1
-url = 'https://myanimelist.net/topanime.php'
-params = {
-    'limit': limit,
-}
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
-res = requests.get(url, headers=headers, params=params)
-soup = BeautifulSoup(res.text, 'html.parser')
-
-#get total pages
-next_page = soup.find('a', {'class': 'link-blue-box next'}).text
 try:
- while (next_page == 'Next 50'):
+ limit = 0
+ no = 1
+ myanimelist = []
+ while (limit < 19250):
   url = 'https://myanimelist.net/topanime.php'
   params = {
       'limit': limit,
@@ -28,7 +18,7 @@ try:
   #get all items
   content = soup.find('table', {'class': 'top-ranking-table'})
   all_content = content.find_all('tr', {'class': 'ranking-list'})
-  myanimelist = []
+
   for item in all_content:
       try:
        rank = item.find('td', {'class': 'rank ac'}).text
@@ -45,7 +35,7 @@ try:
       member = info_eps[3].strip()
       total_member = member.split(' ')[0]
       score = item.find('span', {'class': 'score-label'}).text
-      print(no, title)
+      print(no)
       # sorting data
       data_dict = {
           'no': no,
@@ -58,23 +48,22 @@ try:
           'score': score,
           'link_page': link_page,
       }
-      no += 1
       myanimelist.append(data_dict)
+      no += 1
 
   #get next page
   next_page = soup.find('a', {'class': 'link-blue-box next'}).text
   limit += 50
- else:
-  with open(f'data_json/myanimelist_{limit}.json', 'w') as f:
-   json.dump(myanimelist, f)
-   f.close()
-  print(f'Successfully export {limit} data to json file')
+ with open(f'data_json/myanimelist_{limit}.json', 'w') as f:
+  json.dump(myanimelist, f)
+  f.close()
+ print(f'Successfully export {limit} data to json file')
 #export data to csv & json
-  df = pd.DataFrame(myanimelist)
-  df.to_csv(f'data_csv/myanimelist_{limit}.csv', index=False)
-  print(f'Successfully export {limit} data to csv file')
-  df.to_excel(f'data_excel/myanimelist_{limit}.xlsx', index=False)
-  print(f'Successfully export {limit} data to excel file')
-  print('Done, jumlah anime adalah: ', limit)
+ df = pd.DataFrame(myanimelist)
+ df.to_csv(f'data_csv/myanimelist_{limit}.csv', index=False)
+ print(f'Successfully export {limit} data to csv file')
+ df.to_excel(f'data_excel/myanimelist_{limit}.xlsx', index=False)
+ print(f'Successfully export {limit} data to excel file')
+ print('Done, jumlah anime adalah: ', limit)
 except:
-  print('ada error ', limit)
+  print('ada error ', no)
