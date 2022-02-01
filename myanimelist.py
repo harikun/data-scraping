4c28a5e30b3daee22c6356bf364f13cd0e9ed44e
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-01 19:41:28 +0700

    crawing rank, title, eps, show_time

--- myanimelist.py
+++ myanimelist.py
@@ -10,6 +10,18 @@ soup = BeautifulSoup(res.text, 'html.parser')
 #get total pages
 #get all items
 content = soup.find('table', {'class': 'top-ranking-table'})
-print(content)
+all_content = content.find_all('tr', {'class': 'ranking-list'})
+for item in all_content:
+    rank = item.find('td', {'class': 'rank ac'}).text
+    title = item.find('h3', {'class': 'anime_ranking_h3'}).text
+    link_page = item.find('h3', {'class': 'anime_ranking_h3'}).find('a')['href']
+    info_eps = item.find('div', {'class': 'information'}).text
+    info_eps = info_eps.split('\n')
+    eps = info_eps[1].strip()
+    show_time = info_eps[2].strip()
+    member = info_eps[3].strip()
+    total_member = member.split(' ')[0]
+    score = item.find('td', {'class': 'score ac'}).text
+    print(eps, score)
 #export data to csv & json
 #run
