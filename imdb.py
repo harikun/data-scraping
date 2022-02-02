f48f565c2fb96f1423b8d3233c64f6d1b05f90a2
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-02 18:53:54 +0700

    upload result csv, json and excel file

--- imdb.py
+++ imdb.py
@@ -1,5 +1,7 @@
 # scarping New and Upcoming VOD, DVD, and Blu-ray Releases from imdb.com
+import json
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 url = 'https://www.imdb.com/list/ls016522954/?'
@@ -13,6 +15,7 @@ res = requests.get(url, params=params)
 soup = BeautifulSoup(res.text, 'html.parser')
 
 content = soup.find_all('div', {'class': 'lister-item mode-detail'})
+imdb_list = []
 for item in content:
     item_header = item.find('h3', {'class': 'lister-item-header'})
     no_text = item_header.find('span', {'class': 'lister-item-index'}).text
@@ -34,5 +37,28 @@ for item in content:
     vote_text = star_actor[2].find('span', {'name': 'nv'}).text
     vote = int(vote_text.replace(',', ''))
     release_date = item.find('div', {'class': 'list-description'}).find('p').text.strip()
-    print(no, year)
-    # print(certificate, run_time, genre)
+
+    data_dict = {
+        'no': no,
+        'title': title,
+        'year': year,
+        'certificate': certificate,
+        'run time': run_time,
+        'genre': genre,
+        'rating': rating,
+        'description': description,
+        'director and actor': director_actor,
+        'vote': vote,
+        'release date': release_date
+    }
+    imdb_list.append(data_dict)
+
+with open('data_json/imdb.json', 'w') as f:
+    json.dump(imdb_list, f, indent=4)
+    f.close()
+
+df = pd.DataFrame(imdb_list)
+df.to_csv('data_csv/imdb.csv', index=False)
+print(f'imdb.csv saved')
+df.to_excel('data_excel/imdb.xlsx', index=False)
+print(f'imdb.xlsx saved')
