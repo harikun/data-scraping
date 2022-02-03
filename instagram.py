ffccc7455e54081d62fc8c06d8f84539c902273a
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-03 10:33:58 +0700

    reponse tetap 400 pada instagram, kesimpulannya mmg instagramnya yg membatasi

--- instagram.py
+++ instagram.py
@@ -1,19 +1,21 @@
-from xml.dom.minidom import TypeInfo
 import requests
 import json
 from bs4 import BeautifulSoup
 
+short_code = input('Enter short code: ')
 url = 'https://www.instagram.com/graphql/query/?'
+variables = {
+      'shortcode': short_code,
+      'first': 24,
+}
 params={
       'query_hash': 'd5d763b1e2acf209d62d22d184488e57',
-      'variables': '{"shortcode":"CZY5GBaljKG","include_reel":true,"first":24}'
-}
-headers = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+      'variables': variables
+
 }
 
-res = requests.get(url, params=params, headers=headers)
-print(res.status_code).
+res = requests.get(url, params=params)
+print(res.status_code)
 
-# error 429 - too many requests I think it's because of the api
-# and i need to solve it, but for now i'm just going to sleep for a while
+# error 429 - too many requests I think it's because of the api limit it. new instagram policy i thinks
+
