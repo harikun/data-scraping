0092a9e42eb7af1f2b23ca9ec730fc02b0c2a2eb
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-05 17:27:36 +0700

    code scraping no, company name and address

--- kemenperin.py
+++ kemenperin.py
@@ -9,4 +9,18 @@ params = {
 }
 res = requests.get(url, params=params)
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.prettify())
+
+# pagination = soup.find('ul',{'class': "pagination"})
+# pages = pagination.find_all('li')
+# for page in pages:
+#     page = page.find("a").contents[0].replace('.', '')
+#     res = requests.get(url, params=params)
+#     soup = BeautifulSoup(res.text, 'html.parser')
+
+content = soup.find('tbody')
+all_content = content.find_all('tr')
+for item in all_content:
+ no = item.find('td', {'align': 'right'}).text.replace('.', '')
+ company_name = item.find('td').find_next_sibling('td').find('b').text
+ address = item.find('td').find_next_sibling('td').contents[2].strip()
+ print(no, address)
