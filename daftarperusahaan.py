b084ce4761bd19ea26bad7cb410bb43a59571b6f
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-07 17:33:12 +0700

    try except for fax and telpon

--- daftarperusahaan.py
+++ daftarperusahaan.py
@@ -22,9 +22,9 @@ while (page < 623):
 		content_block = soup.find('div', class_='content clear-block')
 		name = content_block.find('strong').text
 		address = content_block.find('p').text
-		telfon = content_block.find('div', class_='field field-type-text field-field-telepon').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('\n', '').strip()
-		fax = content_block.find('div', class_='field field-type-text field-field-fax').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('\n', '').strip()
 		try:
+			fax = content_block.find('div', class_='field field-type-text field-field-fax').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('\n', '').strip()
+			telfon = content_block.find('div', class_='field field-type-text field-field-telepon').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('\n', '').strip()
 			kode = content_block.find('div', class_='field field-type-text field-field-kode').find('div', class_='field-items').find('div', class_='field-item').text.replace('\n', '').strip()
 			email = content_block.find('div', class_='field field-type-text field-field-email').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('\n', '').strip()
 			website = content_block.find('div', class_='field field-type-text field-field-website').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('\n', '').strip()
@@ -35,6 +35,8 @@ while (page < 623):
 			broker = content_block.find('div', class_='field field-type-text field-field-broker').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('\n', '').strip()
 			npwp = content_block.find('div', class_='field field-type-text field-field-npwp').find('div', class_='field-items').find('div', class_='field-item odd').text.replace('NPWP :', '').strip()
 		except:
+			fax = ''
+			telfon = ''
 			kode = ''
 			email = ''
 			website = ''
@@ -55,7 +57,7 @@ while (page < 623):
 		'npwp' : npwp
 		}
 		company_list.append(data_dict)
-		page += 1
+	page += 1
 	print(page)
 
 with open(f'data_json/daftarperusahaan_{len(company_list)}.json', 'w') as outfile:
