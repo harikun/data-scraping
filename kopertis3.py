99946ae3deb152f42359bce03817dc4180b76bd8
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-09 05:40:36 +0700

    code complete and test

--- kopertis3.py
+++ kopertis3.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 def get_pts():
@@ -9,7 +10,8 @@ def get_pts():
 
 	table = soup.find('table')
 	tr = table.find_all('tr')
-
+	pts = []
+	no = 0
 	for i in range(1, len(tr)):
 		no = int(tr[i].find('td').text.strip())
 		kpdti = int(tr[i].find('td').find_next_sibling('td').text.strip())
@@ -21,6 +23,25 @@ def get_pts():
 			pos_code = ""
 		pts_phone = tr[i].find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').text.strip()
 		fax = tr[i].find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').text.strip()
-		print(fax)
+		pts.append({
+			'no': no,
+			'kpdti': kpdti,
+			'nama pts': pts_name,
+			'alamat': pts_address,
+			'kode pos': pos_code,
+			'telp': pts_phone,
+			'fax': fax
+		})
+
+	with open(f'data_json/pts_kopertis3_{no}.json', 'w') as outfile:
+		json.dump(pts, outfile, indent=4)
+
+	df = pd.DataFrame(pts)
+	df.to_csv(f'data_csv/pts_kopertis3_{no}.csv', index=False)
+
+	df = pd.DataFrame(pts)
+	df.to_excel(f'data_excel/pts_kopertis3_{no}.xlsx', index=False)
+	print(f'{no} pts berhasil di download')
+
 
 get_pts()
