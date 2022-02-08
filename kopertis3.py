e69e8a5b93b452695b376a14e99eb5b7a3d837b9
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-08 22:25:26 +0700

    crawling no, pts name, kdpti

--- kopertis3.py
+++ kopertis3.py
@@ -2,8 +2,19 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://kopertis3.or.id/v2/2010/09/08/alamat-perguruan-tinggi-swasta-no-urut-1-200/'
+def get_pts():
+	url = 'http://kopertis3.or.id/v2/2010/09/08/alamat-perguruan-tinggi-swasta-no-urut-1-200/'
+	res = requests.get(url)
+	soup = BeautifulSoup(res.text, 'html.parser')
 
-res = requests.get(url)
-soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.prettify())
+	table = soup.find('table')
+	tr = table.find_all('tr')
+
+	for i in range(1, len(tr)):
+		no = int(tr[i].find('td').text.strip())
+		kpdti = int(tr[i].find('td').find_next_sibling('td').text.strip())
+		pts_name = tr[i].find('td').find_next_sibling('td').find_next_sibling('td').text.strip()
+
+		print(no, pts_name)
+
+get_pts()
