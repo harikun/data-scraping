1e56ce09a88169bc137187280f2048b80ca071a0
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-12 05:42:26 +0700

    code complete and test to scraping

--- topendsports.py
+++ topendsports.py
@@ -8,6 +8,20 @@ start_time = time.time()
 url = 'https://www.topendsports.com/sport/list/index.htm'
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.prettify())
+sports_list = []
+no = 0
+li = soup.find_all('li')
+for item in li:
+    if no < 900:
+        print(no, item.text)
+    sports_list.append({
+        'no': no,
+        'sport name': item.text
+    })
+    no += 1
+
+with open(f'data_json/sports_list_{no - 1}.json', 'w') as f:
+    json.dump(sports_list, f)
+    f.close()
 
 print("--- %s seconds ---" % (time.time() - start_time))
