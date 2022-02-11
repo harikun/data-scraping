170b16ebc6abd58665d8bad361c1d78d4d3343f1
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-11 21:37:18 +0700

    add code to scraping daftar asosiasi

--- sirekakadin.py
+++ sirekakadin.py
@@ -3,18 +3,21 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = 'http://sireka.kadin-indonesia.com/DaftarSubKlasifikasi//index/NIL/NIL/'
-res = requests.get(url)
+# url = 'http://sireka.kadin-indonesia.com/DaftarSubKlasifikasi//index/NIL/NIL/'
+url2 = 'https://sireka.kadin-indonesia.com/DaftarAsosiasiPerusahaan/index/NIL/NIL/'
+res = requests.get(url2)
 soup = BeautifulSoup(res.text, 'html.parser')
 
 tbody = soup.find('tbody')
 tr = tbody.find_all('tr')
 daftar_sub_klasifikasi = []
-no = 0
+no = 1
 limit = 0
-while limit < 620:
-    url = 'http://sireka.kadin-indonesia.com/DaftarSubKlasifikasi//index/NIL/NIL/' + str(limit)
-    res = requests.get(url)
+# 620
+while limit < 80:
+    # url = 'http://sireka.kadin-indonesia.com/DaftarSubKlasifikasi//index/NIL/NIL/' + str(limit)
+    url2 = 'https://sireka.kadin-indonesia.com/DaftarAsosiasiPerusahaan/index/NIL/NIL/' + str(limit)
+    res = requests.get(url2)
     soup = BeautifulSoup(res.text, 'html.parser')
     tbody = soup.find('tbody')
     tr = tbody.find_all('tr')
@@ -22,23 +25,33 @@ while limit < 620:
         no = no
         td = data.find_all('td')
         kode = td[0].text
-        sub_klasifikasi = td[1].text.strip()
-        print(kode, sub_klasifikasi)
+        # sub_klasifikasi = td[1].text.strip()
+        nama_asosiasi = td[1].text.strip().replace('<br>', '')
+        no_akreditasi = td[2].text.strip()
+        tgl_akreditasi = td[3].text.strip()
+        print(kode, nama_asosiasi)
+        # daftar_sub_klasifikasi.append({
+        #     'no': no,
+        #     'kode': kode,
+        #     'sub_klasifikasi': sub_klasifikasi
+        # })
         daftar_sub_klasifikasi.append({
             'no': no,
             'kode': kode,
-            'sub_klasifikasi': sub_klasifikasi
+            'nama_asosiasi': nama_asosiasi,
+            'no_akreditasi': no_akreditasi,
+            'tgl_akreditasi': tgl_akreditasi
         })
         no += 1
     limit += 20
 
-with open(f'data_json/daftar_sub_klasifikasi_{no}.json', 'w') as f:
+with open(f'data_json/DaftarAsosiasi_{no - 1 }.json', 'w') as f:
     json.dump(daftar_sub_klasifikasi, f)
     f.close()
 
 df = pd.DataFrame(daftar_sub_klasifikasi)
-df.to_csv(f'data_csv/daftar_sub_klasifikasi_{no}.csv', index=False)
+df.to_csv(f'data_csv/DaftarAsosiasi_{no - 1}.csv', index=False)
 
 df = pd.DataFrame(daftar_sub_klasifikasi)
-df.to_excel(f'data_excel/daftar_sub_klasifikasi_{no}.xlsx', index=False)
+df.to_excel(f'data_excel/DaftarAsosiasi_{no - 1}.xlsx', index=False)
 print(f'Successfully export {no}  sub klasifikasi to csv, json and excel file')
