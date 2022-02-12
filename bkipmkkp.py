5a6d9c13dc61088b40897ae1cab7783afb3e5eb1
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-12 21:17:06 +0700

    complate code and test to scraping

--- bkipmkkp.py
+++ bkipmkkp.py
@@ -11,10 +11,32 @@ url = "http://www.bkipm.kkp.go.id/bkipmnew/upifn/index/"
 def importir_terdaftar():
     res = requests.get(url + china)
     soup = BeautifulSoup(res.text, 'html.parser')
+    importir_list = []
+    no = 1
+    table = soup.find('table', {'class': 'table_upi'})
+    tr_upi_data = table.find_all('tr', {'class': 'upi_data'})
+    for td in tr_upi_data:
+        reg_number = td.find('td').text.strip()
+        name_of_establishment = td.find('td').findNextSibling().text
+        address = td.find('td').findNextSibling().findNextSibling().text.strip()
+        products = td.find('td').findNextSibling().findNextSibling().findNextSibling().text.strip()
+        importir_list.append({
+            'no': no,
+            'reg number': reg_number,
+            'name of establishment': name_of_establishment,
+            'address': address,
+            'products': products
+        })
+        no += 1
 
-    table = soup.find('table')
-    tr = table.find_all('tr')
-    print(tr)
+    with open(f'data_json/bkipm_china_{no - 1}.json', 'w') as f:
+        json.dump(importir_list, f, indent=4)
+
+    df = pd.DataFrame(importir_list)
+    df.to_csv(f'data_csv/bkipm_china_{no - 1}.csv', index=False)
+
+    df = pd.DataFrame(importir_list)
+    df.to_excel(f'data_excel/bkipm_china_{no - 1}.xlsx', index=False)
 
 importir_terdaftar()
 
