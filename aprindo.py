61eb55b98e56e544e00cbaf15557ff669adc1d0e
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-13 20:19:22 +0700

    refactoring aprindo.py

--- aprindo.py
+++ aprindo.py
@@ -1,13 +1,9 @@
-import time
-import requests
-from bs4 import BeautifulSoup
-
+import time; import requests; from bs4 import BeautifulSoup
 start_time = time.time()
 url = 'https://www.aprindo.org/profil-aprindo/'
 def download_logo():
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
-
     td_outer_wrap = soup.find('div', {'id': 'td-outer-wrap'})
     td_main_content_wrap = td_outer_wrap.find('div', {'class': 'td-main-content-wrap'})
     td_container = td_main_content_wrap.find('div', {'class': 'td-container'})
@@ -23,7 +19,5 @@ def download_logo():
         with open(f'data_image/aprindo/{img_name}', 'wb') as f:
             f.write(img_res.content)
             f.close()
-
 download_logo()
-
 print("--- %s seconds ---" % (time.time() - start_time))
