0b1dd6c2946dd9eab9e20110e99be95312713fd4
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-14 21:38:23 +0700

    add code to export file in json, csv and excel

--- pertamina.py
+++ pertamina.py
@@ -10,11 +10,18 @@ bard_body = card_bg_white.find('div', {'class': 'card-body'})
 col_lg_9 = bard_body.find('div', {'class': 'col-lg-9'})
 table_responsive = col_lg_9.find_all('div', {'class': 'table-responsive'})
 tr = table_responsive[0].find_all('tr')[1:]
+kapal_list = []; no = 1
 for dt in tr:
     no = dt.find('td').text.strip()
     nama_kapal = dt.find('td').findNextSibling().text.strip()
     pemilik = dt.find('td').findNextSibling().findNextSibling().text.strip()
     keterangan = dt.find('td').findNextSibling().findNextSibling().findNextSibling().text.strip()
-    print(no, nama_kapal, pemilik, keterangan)
-
+    kapal_list.append({
+        'no': no, 'nama kapal': nama_kapal, 'pemilik': pemilik, 'keterangan': keterangan
+    })
+with open(f'data_json/pertamina_{no}.json', 'w') as f:
+    json.dump(kapal_list, f, indent=4)
+df = pd.DataFrame(kapal_list)
+df.to_csv(f'data_csv/pertamina_{no}.csv', index=False)
+df.to_excel(f'data_excel/pertamina_{no}.xlsx', index=False)
 print("--- %s seconds ---" % (time.time() - start_time))
