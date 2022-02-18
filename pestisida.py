46d214ec74241a015f235cf2da32c5edb0c6bd1a
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-19 06:19:22 +0700

    correction on link params

--- pestisida.py
+++ pestisida.py
@@ -1,27 +1,20 @@
 import requests; import pandas as pd; from bs4 import BeautifulSoup; import time
 start_time = time.time()
-url_rekap_formula = 'https://pestisida.id/simpes_app/rekap_formula_nama.php?'
 
-def get_formula(s_kategori, max_page = 1):
-    page = 1; no = 1
-    params = {
-        's_kategori': s_kategori,
-        'rekap_komoditas_formula1Page': page
-    }
-    res = requests.get(url_rekap_formula, params=params)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    tr = soup.find_all('tr', class_='Row')
+def get_formula(s_kategori, max_page):
+    url_rekap_formula = 'https://pestisida.id/simpes_app/rekap_formula_nama.php?'
+    page = 0
     pestisida_list = []
     while page < max_page:
         params = {
         's_kategori': s_kategori,
-        'rekap_komoditas_formula1Page': page
+        'rekap_formula_nama1Page': page
         }
         res = requests.get(url_rekap_formula, params=params)
         soup = BeautifulSoup(res.text, 'html.parser')
         tr = soup.find_all('tr', class_='Row')
         for td in tr:
-            no = int(td.find('td').text)
+            no = td.find('td', {'style' : 'TEXT-ALIGN: right'}).text
             merek_dagang = td.find('td').find_next_sibling('td').contents[0].text.replace('(Umum)', '').replace('(umum)', '').strip()
             bahan_aktif = td.find('td').find_next_sibling('td').contents[3].text.strip()
             deskripsi_singkat = td.find('td').find_next_sibling('td').contents[6].text.strip()
@@ -31,8 +24,12 @@ def get_formula(s_kategori, max_page = 1):
             tanggal_akhir_izin = td.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').contents[6].text.strip()
             no_pendaftaran = td.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').contents[8].text.strip()
             pestisida_list.append({
-                'no': no, 'merek dagang': merek_dagang, 'cara pemakaian': cara_pemakaian, 'pembuat': perusahaan,
+                'no': no,
+                'merek dagang': merek_dagang,
+                'cara pemakaian': cara_pemakaian,
+                'pembuat': perusahaan,
             })
+            print(no, merek_dagang)
         page += 1
     df = pd.DataFrame(pestisida_list)
     df.to_csv(f'data_csv/pestisida_{s_kategori}_{no}.csv', index=False)
