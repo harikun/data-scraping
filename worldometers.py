8ee880ed3823d53625c8c0fc013fbe9369492013
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-21 06:39:19 +0700

    add code to select data no, country, country_link, population_2020, yearly_change, net_change, density_p_km2

--- worldometers.py
+++ worldometers.py
@@ -1,8 +1,17 @@
 import requests; from bs4 import BeautifulSoup; import time; import pandas as pd;
 start_time = time.time()
 url = 'https://www.worldometers.info/world-population/population-by-country/'
+base_url = 'https://www.worldometers.info/'
 res = requests.get(url)
 soup = BeautifulSoup(res.content, 'html.parser')
-tr = soup.find_all('tr')
-for td in tr:
-    print(td.text)
+tbody = soup.find('tbody')
+all_content = tbody.find_all('tr')
+for td in all_content:
+    no = td.find('td').text
+    country = td.find('td').find_next_sibling('td').text
+    country_link = base_url + td.find('td').find_next_sibling('td').find('a').get('href')
+    population_2020 = int(td.find('td').find_next_sibling('td').find_next_sibling('td').text.replace(',', ''))
+    yearly_change = td.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').text
+    net_change = int(td.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').text.replace(',', ''))
+    density_p_km2 = int(td.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').text.replace(',', ''))
+    print(no, density_p_km2)
