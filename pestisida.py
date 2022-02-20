6684d4fe4e54c1ab1022d19c8fe0a385a387a805
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-20 07:00:05 +0700

    remove s_kategori, and change url, comment pestisida_list dictionary and change max_page to 32

--- pestisida.py
+++ pestisida.py
@@ -1,20 +1,26 @@
 import requests; import pandas as pd; from bs4 import BeautifulSoup; import time
 start_time = time.time()
 
-def get_formula(s_kategori, max_page):
+def get_formula(max_page):
     url_rekap_formula = 'https://pestisida.id/simpes_app/rekap_formula_nama.php?'
+    url_rekap_komoditas = 'https://pestisida.id/simpes_app/rekap_komoditas_formula.php?'
     page = 1
     pestisida_list = []
     while page < max_page:
         params = {
-        's_kategori': s_kategori,
-        'rekap_formula_nama1Page': page
+        # 's_kategori': s_kategori,
+        # 'rekap_formula_nama1Page': page
+        'rekap_komoditas_formula1Page': page
         }
-        res = requests.get(url_rekap_formula, params=params)
+        res = requests.get(url_rekap_komoditas, params=params)
         soup = BeautifulSoup(res.text, 'html.parser')
         tr = soup.find_all('tr', class_='Row')
         for td in tr:
-            no = td.find('td', {'style' : 'TEXT-ALIGN: right'}).text
+            no = td.find('td', {'style' : 'TEXT-ALIGN: right'}).text.strip()
+            tujuan = td.find('td').find_next_sibling('td').text
+            kategori = td.find('td').find_next_sibling('td').find_next_sibling('td').text
+            merek = td.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').text
+
             merek_dagang = td.find('td').find_next_sibling('td').contents[0].text.replace('(Ekspor)', '').replace('(ekspor)', '').strip()
             bahan_aktif = td.find('td').find_next_sibling('td').contents[3].text.strip()
             deskripsi_singkat = td.find('td').find_next_sibling('td').contents[6].text.strip()
@@ -25,17 +31,19 @@ def get_formula(s_kategori, max_page):
             no_pendaftaran = td.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').contents[8].text.strip()
             pestisida_list.append({
                 'no': no,
-                'merek dagang': merek_dagang,
-                'bahan aktif': bahan_aktif,
-                'deskrispi': deskripsi_singkat,
-                'pembuat': perusahaan,
+                'merek dagang': merek,
+                'tujuan': tujuan,
+                'kategori': kategori,
+                # 'bahan aktif': bahan_aktif,
+                # 'deskrispi': deskripsi_singkat,
+                # 'pembuat': perusahaan,
             })
-            print(no, merek_dagang)
+            print(no)
         page += 1
     df = pd.DataFrame(pestisida_list)
-    df.to_csv(f'data_csv/pestisida_{s_kategori}_{no}.csv', index=False)
-    df.to_excel(f'data_excel/pestisida_{s_kategori}_{no}.xlsx', index=False)
-    df.to_json(f'data_json/pestisida_{s_kategori}_{no}.json', orient='records')
+    df.to_csv(f'data_csv/pestisida_komoditas_{no}.csv', index=False)
+    df.to_excel(f'data_excel/pestisida_komoditas_{no}.xlsx', index=False)
+    df.to_json(f'data_json/pestisida_komoditas_{no}.json', orient='records')
 #s_kategori yang tersedia ['umum', 'teknis', 'ekspor']
-get_formula('ekspor', 2)
+get_formula(32)
 print("--- %s seconds ---" % (time.time() - start_time))
