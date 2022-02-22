755eea9c15d45f3d85275af0ffdaf7f82a11a3e0
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-22 16:54:34 +0700

    add start-time and f in dataframe conversion so no can read correctly

--- sahamok.py
+++ sahamok.py
@@ -1,5 +1,6 @@
 from wsgiref import headers
 import requests; from bs4 import BeautifulSoup; import pandas as pd; import time;
+start_time = time.time()
 url = 'https://www.sahamok.net/perusahaan-publik-terbuka-tbk-emiten-bei-bursa-efek-indonesia/'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 res = requests.get(url, headers=headers)
@@ -20,6 +21,7 @@ for data in all_tr:
     company = data.find_all('td')[2].text
     code_saham.append({'no' : no, 'Kode Saham' : code, 'Nama Perusahaan' : company})
 df = pd.DataFrame(code_saham)
-df.to_csv('data_csv/sahamok_{no}.csv', index=False)
-df.to_excel('data_excel/sahamok_{no}.xlsx', index=False)
-df.to_json('data_json/sahamok_{no}.json', orient='records')
+df.to_csv(f'data_csv/sahamok_{no}.csv', index=False)
+df.to_excel(f'data_excel/sahamok_{no}.xlsx', index=False)
+df.to_json(f'data_json/sahamok_{no}.json', orient='records')
+print('--- %s seconds ---' % (time.time() - start_time))
