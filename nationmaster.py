93d75303022fec9f3788074f6593702b3cbe180f
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-24 19:37:06 +0700

    add main_battle_tanks, no and data frame to export result in excel, csv, and json

--- nationmaster.py
+++ nationmaster.py
@@ -3,11 +3,15 @@ start_time = time.time()
 url = 'https://www.nationmaster.com/country-info/stats/Military/Army/Main-battle-tanks'
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36' }
 res = requests.get(url, headers=headers)
-soup = BeautifulSoup(res.text, 'html.parser')
+soup = BeautifulSoup(res.content, 'html.parser')
+main_battle_tanks = []; no = 0
 tbody = soup.find('tbody')
 for tr in tbody.find_all('tr'):
+    no += 1
     rangking = tr.find('td').text
     country = tr.find('td').find_next_sibling('td').find('a').find('span', {'class': 'full'}).text.strip()
     tank = tr.find('td').find_next_sibling('td').find_next_sibling('td').text.replace(',', '.').strip()
-    print(rangking, country, tank)
+    year = int(tr.find('td').find_next_sibling('td').find_next_sibling('td').find_next_sibling('td').text.strip())
+    main_battle_tanks.append({ 'no': no, 'rangking' : rangking, 'country' : country, 'number of tank' : tank, 'year' : year })
+df = pd.DataFrame(main_battle_tanks).to_csv(f'data_csv/main_battle_tanks_{no}.csv', index=False).to_excel(f'data_excel/main_battle_tanks_{no}.xlsx', index=False).to_json(f'data_json/main_battle_tanks_{no}.json', orient='records')
 print('--- %s seconds ---' % (time.time() - start_time))
