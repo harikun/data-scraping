6af82a51fa028ccddc8d4e6304e92ad9cfe1bd67
Cipto Suhari <cipto.suheri@gmail.com>
2022-02-27 06:09:16 +0700

    refactor file topendsports.py

--- topendsports.py
+++ topendsports.py
@@ -1,15 +1,10 @@
-import json
-import time
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
+import json; import time; import requests; import pandas as pd; from bs4 import BeautifulSoup
 
 start_time = time.time()
 url = 'https://www.topendsports.com/sport/list/index.htm'
 res = requests.get(url)
 soup = BeautifulSoup(res.content, 'html.parser')
-sports_list = []
-no = 0
+sports_list = []; no = 0
 li = soup.find_all('li')
 for item in li:
     if no < 900:
@@ -19,15 +14,12 @@ for item in li:
             'sport name': item.text
         })
         no += 1
-
 with open(f'data_json/sports_list_{no - 1}.json', 'w') as f:
     json.dump(sports_list, f)
     f.close()
-
 df = pd.DataFrame(sports_list)
 df.to_csv(f'data_csv/sports_list_{no - 1}.csv', index=False)
-
-df = pd.DataFrame(sports_list)
 df.to_excel(f'data_excel/sports_list_{no - 1}.xlsx', index=False)
 
 print("--- %s seconds ---" % (time.time() - start_time))
+print('Support Hari on karyakarsa: https://karyakarsa.com/ciptosuhari')
