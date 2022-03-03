5dab701ec5b22a0aae2c26518366a99296e179b9
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-03 18:06:44 +0700

    import requests; import pandas and beautifulsoup

---
+++ wetv.py
@@ -0,0 +1,7 @@
+import requests; import pandas as pd; from bs4 import BeautifulSoup
+res=requests.get('https://wetv.vip/channel/anime?id=anime.all_content&type=PAGE_TYPE_ALBUM_FILTER')
+soup=BeautifulSoup(res.content,'html.parser')
+module__list = soup.find('div',{'class':'module__list'})
+module__item = module__list.find_all('div',{'class':'module__item'})
+anime_list = []; no = 0
+print(module__item)
