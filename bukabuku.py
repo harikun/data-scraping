8817447e620d8983c8b12cb46832a36b82ef1270
Cipto Suhari <cipto.suheri@gmail.com>
2022-03-06 18:58:12 +0700

    append result scraping to dictionary buku kita soft cover and export to json, csv, and excel

--- bukabuku.py
+++ bukabuku.py
@@ -1,13 +1,21 @@
 import requests; from bs4 import BeautifulSoup; import pandas as pd
-page =1; max_page = 2; buku_kita_populer = []; no = 0; base_url = 'http://www.bukabuku.com'
-res = requests.get(f'http://www.bukabuku.com/browses/index/dept:book/cid:44/format:Soft%20Cover/page:{str(page)}')
-soup = BeautifulSoup(res.text, 'html.parser')
-content = soup.find_all('div', class_='content')
-for item in content:
-    no += 1
-    title = item.find('span', class_='product_list_title').find('a').text
-    author = item.find('div', class_='product_author text_smaller').text.replace('oleh', '').strip()
-    price = int(item.find('span', class_='price').text.replace('Rp.', '').replace('.', '').strip())
-    status_product = item.find('span', class_='product_status_orange').text.strip()
-    link = base_url + item.find('span', class_='product_list_title').find('a').get('href')
-    print(no,title, price, status_product)
+page =1; max_page = 741; buku_kita_softcover = []; no = 0; base_url = 'http://www.bukabuku.com'
+while page <= max_page:
+    res = requests.get(f'http://www.bukabuku.com/browses/index/dept:book/cid:44/format:Soft%20Cover/page:{str(page)}')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = soup.find_all('div', class_='content')
+    for item in content:
+        no += 1
+        title = item.find('span', class_='product_list_title').find('a').text
+        author = item.find('div', class_='product_author text_smaller').text.replace('oleh', '').strip()
+        price = int(item.find('span', class_='price').text.replace('Rp.', '').replace('.', '').strip())
+        status_product = item.find('span', class_='product_status_orange').text.strip()
+        link = base_url + item.find('span', class_='product_list_title').find('a').get('href')
+        print(no,title, price, status_product)
+        buku_kita_populer.append({ 'no': no, 'title': title, 'author': author, 'price': price, 'status_product': status_product, 'link': link})
+    page += 1
+df = pd.DataFrame(buku_kita_populer)
+df.to_csv(f'data_csv/buku_kita_softcover-{no}.csv', index=False)
+df.to_excel(f'data_excel/buku_kita_softcover-{no}.xlsx', index=False)
+df.to_json(f'data_json/buku_kita_softcover-{no}.json', orient='records')
+print('\n Support Hari on karyakarsa: https://karyakarsa.com/ciptosuhari')
